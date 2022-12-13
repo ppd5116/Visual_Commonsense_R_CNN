599cf465371851e1efdb181485c6f93ef5a0a2eb
ppd5116 <48370769+ppd5116@users.noreply.github.com>
2022-12-12 21:37:13 -0500

    Add files via upload

--- checkpoint.py
+++ checkpoint.py
@@ -1,118 +1,139 @@
 # Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved.
-from collections import OrderedDict
+import logging
 import os
-from tempfile import TemporaryDirectory
-import unittest
 
 import torch
-from torch import nn
 
 from vc_rcnn.utils.model_serialization import load_state_dict
-from vc_rcnn.utils.checkpoint import Checkpointer
+from vc_rcnn.utils.c2_model_loading import load_c2_format
+from vc_rcnn.utils.imports import import_file
+from vc_rcnn.utils.model_zoo import cache_url
 
 
-class TestCheckpointer(unittest.TestCase):
-    def create_model(self):
-        return nn.Sequential(nn.Linear(2, 3), nn.Linear(3, 1))
+class Checkpointer(object):
+    def __init__(
+        self,
+        model,
+        optimizer=None,
+        scheduler=None,
+        save_dir="",
+        save_to_disk=None,
+        logger=None,
+    ):
+        self.model = model
+        self.optimizer = optimizer
+        self.scheduler = scheduler
+        self.save_dir = save_dir
+        self.save_to_disk = save_to_disk
+        if logger is None:
+            logger = logging.getLogger(__name__)
+        self.logger = logger
 
-    def create_complex_model(self):
-        m = nn.Module()
-        m.block1 = nn.Module()
-        m.block1.layer1 = nn.Linear(2, 3)
-        m.layer2 = nn.Linear(3, 2)
-        m.res = nn.Module()
-        m.res.layer2 = nn.Linear(3, 2)
+    def save(self, name, **kwargs):
+        if not self.save_dir:
+            return
 
-        state_dict = OrderedDict()
-        state_dict["layer1.weight"] = torch.rand(3, 2)
-        state_dict["layer1.bias"] = torch.rand(3)
-        state_dict["layer2.weight"] = torch.rand(2, 3)
-        state_dict["layer2.bias"] = torch.rand(2)
-        state_dict["res.layer2.weight"] = torch.rand(2, 3)
-        state_dict["res.layer2.bias"] = torch.rand(2)
+        if not self.save_to_disk:
+            return
 
-        return m, state_dict
+        data = {}
+        data["model"] = self.model.state_dict()
+        if self.optimizer is not None:
+            data["optimizer"] = self.optimizer.state_dict()
+        if self.scheduler is not None:
+            data["scheduler"] = self.scheduler.state_dict()
+        data.update(kwargs)
 
-    def test_from_last_checkpoint_model(self):
-        # test that loading works even if they differ by a prefix
-        for trained_model, fresh_model in [
-            (self.create_model(), self.create_model()),
-            (nn.DataParallel(self.create_model()), self.create_model()),
-            (self.create_model(), nn.DataParallel(self.create_model())),
-            (
-                nn.DataParallel(self.create_model()),
-                nn.DataParallel(self.create_model()),
-            ),
-        ]:
+        save_file = os.path.join(self.save_dir, "{}.pth".format(name))
+        self.logger.info("Saving checkpoint to {}".format(save_file))
+        torch.save(data, save_file)
+        self.tag_last_checkpoint(save_file)
 
-            with TemporaryDirectory() as f:
-                checkpointer = Checkpointer(
-                    trained_model, save_dir=f, save_to_disk=True
-                )
-                checkpointer.save("checkpoint_file")
+    def load(self, f=None, use_latest=True):
+        if self.has_checkpoint() and use_latest:
+            # override argument with existing checkpoint
+            f = self.get_checkpoint_file()
+        if not f:
+            # no checkpoint could be found
+            self.logger.info("No checkpoint found. Initializing model from scratch")
+            return {}
+        self.logger.info("Loading checkpoint from {}".format(f))
+        checkpoint = self._load_file(f)
+        self._load_model(checkpoint)
+        if "optimizer" in checkpoint and self.optimizer:
+            self.logger.info("Loading optimizer from {}".format(f))
+            self.optimizer.load_state_dict(checkpoint.pop("optimizer"))
+        if "scheduler" in checkpoint and self.scheduler:
+            self.logger.info("Loading scheduler from {}".format(f))
+            self.scheduler.load_state_dict(checkpoint.pop("scheduler"))
 
-                # in the same folder
-                fresh_checkpointer = Checkpointer(fresh_model, save_dir=f)
-                self.assertTrue(fresh_checkpointer.has_checkpoint())
-                self.assertEqual(
-                    fresh_checkpointer.get_checkpoint_file(),
-                    os.path.join(f, "checkpoint_file.pth"),
-                )
-                _ = fresh_checkpointer.load()
+        # return any further checkpoint data
+        return checkpoint
 
-            for trained_p, loaded_p in zip(
-                trained_model.parameters(), fresh_model.parameters()
-            ):
-                # different tensor references
-                self.assertFalse(id(trained_p) == id(loaded_p))
-                # same content
-                self.assertTrue(trained_p.equal(loaded_p))
+    def has_checkpoint(self):
+        save_file = os.path.join(self.save_dir, "last_checkpoint")
+        return os.path.exists(save_file)
 
-    def test_from_name_file_model(self):
-        # test that loading works even if they differ by a prefix
-        for trained_model, fresh_model in [
-            (self.create_model(), self.create_model()),
-            (nn.DataParallel(self.create_model()), self.create_model()),
-            (self.create_model(), nn.DataParallel(self.create_model())),
-            (
-                nn.DataParallel(self.create_model()),
-                nn.DataParallel(self.create_model()),
-            ),
-        ]:
-            with TemporaryDirectory() as f:
-                checkpointer = Checkpointer(
-                    trained_model, save_dir=f, save_to_disk=True
-                )
-                checkpointer.save("checkpoint_file")
+    def get_checkpoint_file(self):
+        save_file = os.path.join(self.save_dir, "last_checkpoint")
+        try:
+            with open(save_file, "r") as f:
+                last_saved = f.read()
+                last_saved = last_saved.strip()
+        except IOError:
+            # if file doesn't exist, maybe because it has just been
+            # deleted by a separate process
+            last_saved = ""
+        return last_saved
 
-                # on different folders
-                with TemporaryDirectory() as g:
-                    fresh_checkpointer = Checkpointer(fresh_model, save_dir=g)
-                    self.assertFalse(fresh_checkpointer.has_checkpoint())
-                    self.assertEqual(fresh_checkpointer.get_checkpoint_file(), "")
-                    _ = fresh_checkpointer.load(os.path.join(f, "checkpoint_file.pth"))
+    def tag_last_checkpoint(self, last_filename):
+        save_file = os.path.join(self.save_dir, "last_checkpoint")
+        with open(save_file, "w") as f:
+            f.write(last_filename)
 
-            for trained_p, loaded_p in zip(
-                trained_model.parameters(), fresh_model.parameters()
-            ):
-                # different tensor references
-                self.assertFalse(id(trained_p) == id(loaded_p))
-                # same content
-                self.assertTrue(trained_p.equal(loaded_p))
+    def _load_file(self, f):
+        return torch.load(f, map_location=torch.device("cpu"))
 
-    def test_complex_model_loaded(self):
-        for add_data_parallel in [False, True]:
-            model, state_dict = self.create_complex_model()
-            if add_data_parallel:
-                model = nn.DataParallel(model)
-
-            load_state_dict(model, state_dict)
-            for loaded, stored in zip(model.state_dict().values(), state_dict.values()):
-                # different tensor references
-                self.assertFalse(id(loaded) == id(stored))
-                # same content
-                self.assertTrue(loaded.equal(stored))
+    def _load_model(self, checkpoint):
+        load_state_dict(self.model, checkpoint.pop("model"))
 
 
-if __name__ == "__main__":
-    unittest.main()
+class DetectronCheckpointer(Checkpointer):
+    def __init__(
+        self,
+        cfg,
+        model,
+        optimizer=None,
+        scheduler=None,
+        save_dir="",
+        save_to_disk=None,
+        logger=None,
+    ):
+        super(DetectronCheckpointer, self).__init__(
+            model, optimizer, scheduler, save_dir, save_to_disk, logger
+        )
+        self.cfg = cfg.clone()
+
+    def _load_file(self, f):
+        # catalog lookup
+        if f.startswith("catalog://"):
+            paths_catalog = import_file(
+                "vc_rcnn.config.paths_catalog", self.cfg.PATHS_CATALOG, True
+            )
+            catalog_f = paths_catalog.ModelCatalog.get(f[len("catalog://") :])
+            self.logger.info("{} points to {}".format(f, catalog_f))
+            f = catalog_f
+        # download url files
+        if f.startswith("http"):
+            # if the file is a url path, download it and cache it
+            cached_f = cache_url(f)
+            self.logger.info("url {} cached in {}".format(f, cached_f))
+            f = cached_f
+        # convert Caffe2 checkpoint from pkl
+        if f.endswith(".pkl"):
+            return load_c2_format(self.cfg, f)
+        # load native detectron.pytorch checkpoint
+        loaded = super(DetectronCheckpointer, self)._load_file(f)
+        if "model" not in loaded:
+            loaded = dict(model=loaded)
+        return loaded
